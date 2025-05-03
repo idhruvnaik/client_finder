40857528b304c447477d307593cfb1375b612c06
idhruvnaik <dhruv97naik@gmail.com>
2025-05-03 18:37:26 +0530

    Feature : Client parser

---
+++ app/services/client_service/parser.rb
@@ -0,0 +1,22 @@
+module ClientService
+  class Parser
+    attr_reader :clients
+
+    def initialize(file:)
+      @file = file
+      @clients = []
+    end
+
+    def parse
+      begin
+        @file.read.each_line do |line|
+          data = JSON.parse(line, symbolize_names: true)
+          @clients << Client.new(user_id: data[:user_id], name: data[:name], latitude: data[:latitude], longitude: data[:longitude])
+        end
+      rescue => error
+        Rails.logger.error("Something went wrong Client::Parser#parse #{e.message}")
+        raise error
+      end
+    end
+  end
+end
